43a090fd13a91c05557c208c528bac52537fc99a
Nishanko, Nishant <Nishant_Nishanko@intuit.com>
2017-04-29 15:49:47 -0700

    generic addAlarm function which registeres an UNNotification with sound

--- DeFuge/ViewController.swift
+++ DeFuge/ViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let alarm = Alarm()
+        alarm.time = Date().addingTimeInterval(20.0)
+        addAlarm(alarm: alarm)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addAlarm(alarm: Alarm){
+        // ask permission for notification
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (authorized, error) in
+            if authorized {
+                print("access granted, proceed")
+                var date = DateComponents()
+                let calendar = Calendar.current
+                
+                let hour = calendar.component(.hour, from: alarm.time)
+                let minutes = calendar.component(.minute, from: alarm.time)
+                let seconds = calendar.component(.second, from: alarm.time)
+                
+                date.hour = hour
+                date.minute = minutes
+                date.second = seconds
+                let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+
+                let content = UNMutableNotificationContent()
+                content.categoryIdentifier = "defugue"
+                content.title = "Alarm"
+                content.body = "wakey wakey"
+                content.userInfo = ["customNumber": 100, "time": "hours = \(hour):\(minutes):\(seconds)"]
+                content.sound = UNNotificationSound(named: "Elegant.mp3")
+                
+                let request = UNNotificationRequest(identifier: "exampleNotification", content: content, trigger: trigger)
+                center.add(request, withCompletionHandler: nil)
+            }
+        }
     }
 
 
